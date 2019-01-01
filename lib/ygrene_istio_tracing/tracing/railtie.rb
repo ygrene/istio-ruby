a54826ec590282d718cda346fbf7a0cff8deea77
Sergey Moiseev <moiseev.sergey@gmail.com>
2019-01-01 16:52:27 +0200

    Fix middleware load

--- lib/ygrene_istio_tracing/tracing/railtie.rb
+++ lib/ygrene_istio_tracing/tracing/railtie.rb
@@ -5,7 +5,7 @@ require 'rails/railtie'
 module YgreneIstioTracing
   module Tracing
     class Railtie < ::Rails::Railtie
-      initializer 'ygrene_istio_tracing.tracing.rack_middleware' do |app|
+      initializer 'ygrene_istio_tracing.insert_middleware' do |app|
         puts 'Tracing Loaded'
         app.middleware.use(::YgreneIstioTracing::Tracing::RackMiddleware)
       end
